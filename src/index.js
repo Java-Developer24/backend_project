5e69f5527752c61c831213b097413e899bf51dd0
Java-Developer24 <balu2446madhu@gmail.com>
2024-09-04 10:08:08 +0530

    v2

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 import mongoose from "mongoose";
 import express from "express";
-import { DB_NAME } from "./constants";
+// import { DB_NAME } from "./constants.js";
 
 const app=express();
-
+const PORT=process.env.PORT||8000
+const MONGODB_URL = process.env.MONGODB_URL ||"mongodb+srv://balu:balu@cluster0.1pebo.mongodb.net"
+const DB_NAME ="videotube";
 
 (async ()=>{
     try {
-        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-        app.on()
+        console.log('Starting application...');
+
+        const instanceConnection=await mongoose.connect(`${MONGODB_URL}/${DB_NAME}`)
+        console.log(instanceConnection.connection.readyState);
+    
+        app.on("error",(error)=>{
+            console.log("Error",error)
+            throw error
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`app is listening on port ${process.env.PORT}`)
+        })
         
     } catch (error) {
         console.log(error)
-        process.exit(1)
+        throw error
         
     }
 })()
