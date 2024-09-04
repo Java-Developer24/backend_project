8b28c13a2be1a8e20ffb6033d6efbc6cd4afa657
Java-Developer24 <balu2446madhu@gmail.com>
2024-09-04 10:50:01 +0530

    com

--- src/index.js
+++ src/index.js
@@ -1,30 +1,67 @@
-import mongoose from "mongoose";
-import express from "express";
-// import { DB_NAME } from "./constants.js";
+// require('dotenv').config()
+import dotenv from 'dotenv'
+import connectDB from './DB/index.js'
 
-const app=express();
-const PORT=process.env.PORT||8000
-const MONGODB_URL = process.env.MONGODB_URL ||"mongodb+srv://balu:balu@cluster0.1pebo.mongodb.net"
-const DB_NAME ="videotube";
 
-(async ()=>{
-    try {
-        console.log('Starting application...');
+dotenv.config({
+    path:"./env"
+})
 
-        const instanceConnection=await mongoose.connect(`${MONGODB_URL}/${DB_NAME}`)
-        console.log(instanceConnection.connection.readyState);
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//second way of connecting database 
+// import mongoose from "mongoose";
+// import express from "express";
+// // import { DB_NAME } from "./constants.js";
+
+// const app=express();
+// const PORT=process.env.PORT||8000
+// const MONGODB_URI = "mongodb+srv://balu:balu@cluster0.1pebo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+// const DB_NAME ="videotube";
+
+// (async ()=>{
+//     try {
+//         console.log('Starting application...');
+
+//         const instanceConnection=await mongoose.connect(`${MONGODB_URI}/${DB_NAME}`)
+//         console.log(instanceConnection.connection.readyState);
     
-        app.on("error",(error)=>{
-            console.log("Error",error)
-            throw error
-        })
-        app.listen(process.env.PORT,()=>{
-            console.log(`app is listening on port ${process.env.PORT}`)
-        })
+//         app.on("error",(error)=>{
+//             console.log("Error",error)
+//             throw error
+//         })
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`app is listening on port ${process.env.PORT}`)
+//         })
         
-    } catch (error) {
-        console.log(error)
-        throw error
+//     } catch (error) {
+//         console.log(error)
+//         throw error
         
-    }
-})()
+//     }
+// })()
