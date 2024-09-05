edf85f235df061a111d2422c316b9e0e34fbde9b
Developer@2446 <105372372+Java-Developer24@users.noreply.github.com>
2024-09-05 03:51:48 +0000

    teset

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 // require('dotenv').config()
 import dotenv from 'dotenv'
 import connectDB from './DB/index.js'
+import app from './app.js'
 
 
 dotenv.config({
     path:"./env"
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log(`server is listening at  port ${process.env.PORT}` );
+        
+    })
+})
+.catch((err)=>{
+    console.log(err);
+    
+})
 
 
 
