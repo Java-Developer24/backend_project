5cd255ec6be1c1c319a653390b75476665c2f009
Developer@2446 <105372372+Java-Developer24@users.noreply.github.com>
2024-09-05 06:59:03 +0000

    jwt

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose"
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
  const videoSchema=new mongoose.Schema({
     videofile:{
         type:String,
@@ -36,4 +37,5 @@ import mongoose from "mongoose"
 
  },{timestamps:true})
 
+ videoSchema.plugin(mongooseAggregatePaginate)
  export const Video=mongoose.model("Video",videoSchema)
